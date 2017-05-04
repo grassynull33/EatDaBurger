3c20dd2ded9e04e4fbf5552cacb7b009438444d4
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-05-03 18:27:52 -0700

    orm.js

---
+++ config/orm.js
@@ -0,0 +1,34 @@
+var connection = require("./connection.js");
+
+var orm = {
+	selectAll: function(tableInput) {
+		var queryString = "SELECT * from ??";
+		connection.query(queryString, [tableInput]. function(err, res) {
+			if (err) {
+				throw err;
+			}
+			console.log(res);
+			return res;
+		});
+	},
+	insertOne: function(tableInput, columnName, burgerName) {
+		var queryString = "INSERT INTO ?? (??) VALUES (?)";
+		connection.query(queryString, [tableInput, columnName, burgerName], function(err, res) {
+			if (err) {
+				throw err;
+			}
+			console.log(res);
+		});
+	},
+	updateOne: function(tableInput, updateColumnName, rowValue, searchColumnName, burgerName) {
+		var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+		connection.query(queryString, [tableInput, updateColumnName, rowValue, searchColumnName, burgerName], function(err, res) {
+			if (err) {
+				throw err;
+			}
+			console.log(res);
+		});
+	}
+};
+
+module.exports = orm;
